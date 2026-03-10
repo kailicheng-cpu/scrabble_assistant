fa2e8e30d949701bd5421d9bdbb5efc7d31ae649
kailicheng-cpu <kaili.cheng@ashbury.ca>
2026-03-10 16:22:38 -0400

    Update check_and_sort_dictionary.py
    This is the fixed commenting of the check and sort algorithm

--- check_and_sort_dictionary.py
+++ check_and_sort_dictionary.py
@@ -6,16 +6,20 @@ Then checking and removing any duplicate words
 Finally Writing the Words back into the text file
 '''
 
-# TODO: Make this sort the file properly
 
 counter = 0
 
 with open('dictionary.txt', 'r') as file:
+
+    # Reads each line of the file and saves each line as a value in a list
     word_dict = [line.strip() for line in file]
 
     word_dict.sort()
 
+    # Compares the current and next value in the list
     while counter < (len(word_dict) - 1):
+
+        # If Values are the same, the next one gets removed
         if word_dict[counter] == word_dict[counter+1]:
             word_dict.pop(counter+1)
 
@@ -23,6 +27,7 @@ with open('dictionary.txt', 'r') as file:
             counter +=1
     print(word_dict)
 
+# Writes in each value, of the now sorted and checked list, on a line
 with open('dictionary.txt', 'w') as file:
     for i in word_dict:
         file.write( i + "\n")
