626e753fd1bf7e7544e0facf7544ba0bc6e69401
kailicheng-cpu <kaili.cheng@ashbury.ca>
2026-03-24 22:08:46 -0400

    Non-tested word generating program
    Recursive combo building program

--- WordFinder.py
+++ WordFinder.py
@@ -11,17 +11,49 @@ output: list of valid playable words
 Note:
 Use a set for the generated possible combinations, because a set removes duplicate combination of letters
 '''
+
+from dictionary_manager import DictionaryManager
+
 class WordFinder:
     def __init__(self):
-        self.file_name = 'dictionary.txt'
-        self.all_word = []
+        self.dictionary_words = None
+        self.generated_combinations = set()
+
+        self.varified_words = []
 
         self.load_words()
 
     def load_words(self):
-        with open(self.file_name, 'r') as file:
+        self.dict_manager = DictionaryManager()
+        self.dictionary_words = self.dict_manager.get_word_list()
+        print(self.dictionary_words)
 
-            # Reads each line of the file and saves each line as a value in a list
-            self.all_words = [line.strip() for line in file]
+    def generate_combo(self, tiles:list):
+        tiles = (t.upper() for t in tiles)
 
-    def
+        # starting off the recursive method empty
+        self.combo_building("", tiles)
+
+        print(self.generated_combinations)
+
+    def combo_building(self, current_combo, remaining_tiles:list):
+
+        #adding the current combo to the set of results if not empty
+        if current_combo:
+            self.generated_combinations.add(current_combo)
+
+        else:
+            for i in range(len(remaining_tiles)):
+                letter = remaining_tiles[i]
+
+                new_combo = current_combo + letter
+                new_remainder = remaining_tiles[:i]+remaining_tiles[i:]
+
+                self.combo_building(new_combo, new_remainder)
+
+
+# making sure words are being loaded correctly
+wordfinder = WordFinder()
+wordfinder.load_words()
+
+wordfinder.generate_combo(['c', 'A', 'T'])
