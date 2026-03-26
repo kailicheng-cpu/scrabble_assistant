cfe4dee110c83d49b73ae7f67ebaae0369cf4fa1
finlayspratt-blip <finlay.spratt@ashbury.ca>
2026-03-26 10:44:20 -0400

    Updated GUI progress
    The third screen was created which includes a filter and the words generated

--- GUI.py
+++ GUI.py
@@ -3,9 +3,13 @@ import tkinter as tk
 def show_frame(frame):
     frame.tkraise()
 
+def add_wild():
+    enter_text.insert(tk.END, "*")
+
 root = tk.Tk()
 root.geometry("1400x800") 
 
+score = 0
 
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
@@ -37,15 +41,42 @@ for i in range(3):
 label2 = tk.Label(frame2, font=("Courier", 60), text="Enter letters:", bg="lightblue")
 label2.grid(row=0, column=1, sticky="nsew", padx=(220, 400), pady=(200, 0))
 
+score_label = tk.Label(frame2, font=("Courier", 55), text="Score:", bg="lightblue")
+score_label.grid(row=1, column=1, sticky="nsew", padx=(0, 1130), pady=(450, 20))
+
+score_text = tk.Entry(frame2, font=("Courier", 60))
+score_text.grid(row=1, column=1, sticky="nsew", padx=(250, 900), pady=(450, 20))
+
 enter_text = tk.Entry(frame2, font=("Courier", 60))
 enter_text.grid(row=1, column=1, sticky="nsew", padx=350, pady=(10, 430))
 
-solve_button = tk.Button(frame2, text="Solve", command=lambda: show_frame(frame1), font=("Courier", 30, "bold"))
+solve_button = tk.Button(frame2, text="Solve", command=lambda: show_frame(frame3), font=("Courier", 30, "bold"))
 solve_button.grid(row=1, column=1, sticky="nsew", padx=(500, 500), pady=(110, 360))
 
-wild_button = tk.Button(frame2, text="Add\nWild", font=("Courier", 30, "bold"))
+wild_button = tk.Button(frame2, text="Add\nWild", command=add_wild, font=("Courier", 30, "bold"))
 wild_button.grid(row=1, column=1, sticky="nsew", padx=(1065, 230), pady=(10, 430))
 
+# ----------------- Frame 3 -----------------
+frame3 = tk.Frame(root, bg="lightblue")
+frame3.grid(row=0, column=0, sticky="nsew")
+
+label3 = tk.Label(frame3, font=("Courier", 60), text="Words:", bg="lightblue")
+label3.grid(row=0, column=0, sticky="nsew", padx=(0, 1350), pady=(30, 800))
+
+filter_label = tk.Label(frame3, font=("Courier", 55), text="Filter Category", bg="lightblue")
+filter_label.grid(row=0, column=0, sticky="nsew", padx=(820, 300), pady=(30, 800))
+
+Highest_button = tk.Button(frame3, text="Highest Score", font=("Courier", 30, "bold"))
+Highest_button.grid(row=0, column=0, sticky="nsew", padx=(820, 300), pady=(130, 700))
+
+Longest_button = tk.Button(frame3, text="Longest Word", font=("Courier", 30, "bold"))
+Longest_button.grid(row=0, column=0, sticky="nsew", padx=(820, 300), pady=(230, 600))
+
+Shortest_button = tk.Button(frame3, text="Shortest Word", font=("Courier", 30, "bold"))
+Shortest_button.grid(row=0, column=0, sticky="nsew", padx=(820, 300), pady=(330, 500))
+
+Contains_button = tk.Button(frame3, text="Contains letter:", font=("Courier", 30, "bold"))
+Contains_button.grid(row=0, column=0, sticky="nsew", padx=(820, 430), pady=(430, 400))
 
 frame1.tkraise()
 
